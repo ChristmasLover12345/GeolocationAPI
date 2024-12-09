337f4ac8d4ef8f13109c96c357aac189af99811f
ChristmasLover12345 <madnes0205@gmail.com>
2024-12-09 10:13:01 -0800

    o algo

--- scripts/app.js
+++ scripts/app.js
@@ -31,7 +31,7 @@ function errorFunc(error){
 success()
 
 function apiCall() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${APIKEY}`)
+    fetch(`api.openweathermap.org/data/2.5/forecast?lat=52.3676&lon=4.9041&appid={API key}${APIKEY}`)
     .then((response) => {
         return response.json()
     })
