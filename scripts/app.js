51a3940181e84d8b507fc95571f1f77ccf370c2a
ChristmasLover12345 <madnes0205@gmail.com>
2024-12-09 09:42:39 -0800

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,43 @@
+import { APIKEY } from "./environment.js";
+
+
+// this is a built in API that will allow the user to get their current location upon accepting the prompt
+// navigator returms the geolocation object
+// getCurrentPosition() returns the current position of the user
+navigator.geolocation.getCurrentPosition( success. errorFunc);
+// think of this as a if else statement. if the user accepts it is succesful, if not its an error
+
+// Example of a geolocation object
+{
+    cords: {
+        latitude: 37.3214;
+        longitude: -24,1243;
+    }
+}
+
+
+function success(position){
+    console.log(position);
+    console.log("Our latitude is: " + position.coords.latitude);
+    console.log("Our latitude is: " + position.coords.longitude);
+    console.log("Get doxxed brah");
+}
+
+
+function errorFunc(error){
+    console.log(error.message)
+}
+
+success()
+
+function apiCall() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${APIKEY}`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data);
+    })
+}
+
+apiCall()
